d62b84ce9a304ac2e0bb3bb1374067d74465a5d6
Joshua de Lange <joshua.delange@gmail.com>
2015-04-06 15:22:22 +0200

    humblebeginnings

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	var colors = ['#EE4035', '#F37737', '#F9E97A', '#7AC043', '#9ED9F7'];
+	var randomColor = function(){
+		return colors[Math.floor(Math.random() * colors.length) ];
+	};
+	$.ajax({
+		url: 'https://www.kimonolabs.com/api/5h87ns5c?apikey=ade40a23fb70523f951a0d582180bf3f',
+		crossDomain: true,
+		dataType: 'jsonp',
+		success: function (response) {
+			console.log(response.results.collection1);
+			$.each(response.results.collection1, function(index, item){
+				$('#tweets-here-please').append('<span style="background-color:' + randomColor() + '">' + item.tweet + '</span>');
+			});
+			
+		}
+	});
+});
